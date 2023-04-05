aefee69c88098e9ad899ccdca275d1a712f38c04
Shaurya10101 <shauryagupta8046@gmail.com>
2023-04-05 16:11:26 +0530

    typing game repo

---
+++ Typing-game/script.js
@@ -0,0 +1,10 @@
+let var1=document.getElementById("start")
+let para=document.getElementById("message")
+let count=0
+function Increment(){
+    //let para=document.getElementById("message")
+    count=count+1
+    //para.innerHTML="count";
+    para.innerText=count;
+    console.log(para)
+}
